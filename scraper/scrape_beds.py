89525791936902705fd0678a183aa781e77beebf
Louis Hitchcock <48716762+LouisHitchcock@users.noreply.github.com>
2024-09-23 11:25:49 +0100

    Fixed Duplicate Rehabs

--- scraper/scrape_beds.py
+++ scraper/scrape_beds.py
@@ -7,9 +7,8 @@ import os
 # List of URLs to scrape along with their respective purposes
 URLS = [
     ("https://www.cygnetgroup.com/professionals/bed-placement-search/?select-service=health-care-services&service=84&social_care_service=&gender=all", "Rehab"),
-    ("https://www.cygnetgroup.com/professionals/bed-placement-search/?select-service=&service=87&social_care_service=&gender=all", "PDU"),
-    ("https://www.cygnetgroup.com/professionals/bed-placement-search/?select-service=&service=81&social_care_service=&gender=all", "Acute/PICU"),
-    ("https://www.cygnetgroup.com/professionals/bed-placement-search/?select-service=&service=84&social_care_service=&gender=all", "Rehab")
+    ("https://www.cygnetgroup.com/professionals/bed-placement-search/?select-service=health-care-services&service=87&social_care_service=&gender=all", "PDU"),
+    ("hhttps://www.cygnetgroup.com/professionals/bed-placement-search/?select-service=health-care-services&service=81&social_care_service=&gender=all", "Acute/PICU")
 ]
 
 def get_bed_availability():
